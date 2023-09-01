fc8cc45bbce2c85f29d7e0a1aedb31d5a24266ef
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2023-09-01 09:48:49 -0500

    calculations?

--- Mod 1 Creative App_Kaiser/ViewController.swift
+++ Mod 1 Creative App_Kaiser/ViewController.swift
@@ -11,12 +11,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var unemploymentFieldOutlet: UILabel!
     @IBOutlet weak var employmentFieldOutlet: UITextField!
-    
+    @IBOutlet weak var employmentRateOutlet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func calculateAction(_ sender: Any) {
+        var unemployment = Int(unemploymentFieldOutlet.text!)
+        var employment = Int(employmentFieldOutlet.text!)
+        if let a = unemployment {
+            if let a = employment {
+             var rate = (Double(unemployment!)/Double(employment!)) * 100
+                employmentRateOutlet.text = "The Employment Rate: \(rate)"
+            } else {
+                employmentRateOutlet.text = "Invalid Data"
+            }
+        } else{
+            employmentRateOutlet.text = "Invalid Data"
+        }
+        
+    }
     
     
     
